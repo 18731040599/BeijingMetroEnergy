af23556ac8c8a835eb026adcc1d7c3c70b207754
WangYuhang <wangyuhang01@thtf.com.cn>
2018-11-22 17:40:27 +0800

    TXT文件导入hbase

--- src/com/thtf/sparktest/MyTest.scala
+++ src/com/thtf/sparktest/MyTest.scala
@@ -4,84 +4,88 @@ import org.apache.hadoop.hbase.util.Bytes
 import java.util.Date
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.io.File
+import java.io.FilenameFilter
 
 case class MyUser(
-    name: String,
-    age: Int
-    )
+  name: String,
+  age: Int)
 object MyTest {
   def main(args: Array[String]): Unit = {
+
+    try {
+      val i = 1 / 0
+      println("start")
+    } catch {
+      case t: Throwable => t.printStackTrace() // TODO: handle error
+    }
+    println("done")
+
+    val dateStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date)
+    val date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateStr)
+    println(new Date)
+    println(dateStr)
+    val calendar = Calendar.getInstance
+    val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:m:00")
+    val endTime = simpleDateFormat.format(calendar.getTime)
+    //Thread.sleep(1000*60)
+    calendar.add(Calendar.MINUTE, -20)
+    val startTime = simpleDateFormat.format(calendar.getTime)
+    calendar.add(Calendar.MINUTE, -(20 * 71))
+    val lastDay = simpleDateFormat.format(calendar.getTime)
+    println(endTime)
+    println(startTime)
+    println(lastDay)
+    println(endTime == "2018-11-14 15:37:00")
+
+    val map = Map("x" -> "x")
+
+    val s = s"""row${"%03d".format(1)}"""
+    val s1 = s"""row${"%010d".format(4)}"""
+    println(s)
+    println(s1)
     
-    println("—" == "—")
+    val file = new File("F:/java/mysql")
+    val files = file.list(new FilenameFilter(){
+      def accept(dir: File, name: String): Boolean = {
+        if(name.startsWith("p")){
+          return true
+        }
+        return false
+      }
+    })
+    files.map(println(_))
     
-    val x = "bc"
-    val y = "e"
-    val z = s"${x}d${y}"
-    println(z)
+    val aaa = "aaa.txt"
+    aaa.dropRight(4)
+    println(aaa)
     
-   try {
-     val i = 1/0
-     println("start")
-   } catch {
-     case t: Throwable => t.printStackTrace() // TODO: handle error
-   }
-   println("done")
-   
-   val dateStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date)
-   val date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateStr)
-   println(new Date)
-   println(dateStr)
-   val calendar = Calendar.getInstance
-        val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:00")
-        val endTime = simpleDateFormat.format(calendar.getTime)
-        //Thread.sleep(1000*60)
-        calendar.add(Calendar.MINUTE, -20)
-        val startTime = simpleDateFormat.format(calendar.getTime)
-        calendar.add(Calendar.MINUTE, -(20*71))
-        val lastDay = simpleDateFormat.format(calendar.getTime)
-        println(endTime)
-        println(startTime)
-        println(lastDay)
-        println(endTime == "2018-11-14 15:37:00")
-        
-   val map = Map("x" -> "x")
-   
-   println(100./(20).*(5))
-   println(Math.abs(-6))
-   
-   val a = MyUser.apply("Li", 18)
-   println(a)
-   val b = MyUser.apply("Wang", 20)
-   println(a)
-   println(b)
+    
     
     /*
     val str = "123,,12,33"
     str.split(",").foreach(println(_))
-    
-    val line = "九号线	七里庄	KT_A1_FreqFB_Hz	YC	0	KT_A1	A1送风机	DI	1		1	0	50	送风机1频率反馈".split("\t")
-    println(line.length)
-    
-    
+
     val s = s"""row${"%03d".format(1)}"""
     println(s)
-    
+
     def i = 1
     println(i + "" + i.getClass())
-    
+
     val t1 = "123,456,789"
-    val t2 = """This is 
+    val t2 = """This is
                |a multiline
                |String""".stripMargin
     println(t1)
     println(t2)
-    
+
     println("----------")
     println("f:age".hashCode())
     println("rowkey".hashCode())
     println("f:name".hashCode())
     println("f:password".hashCode())
     */
+    
     /*
      val columns_type = Map[String,String]("id" -> "int",
                            "name" -> "string",
@@ -89,45 +93,17 @@ object MyTest {
                            "age" -> "int")
     println(columns_type.get("id"))
     println(columns_type.getOrElse("id",""))
-    
+
     Bytes.toBytes(16).foreach(x => {print(x.toChar.toString())})
     */
-    
-    /*//val point = Bytes.toInt(null);
-    val a = Array(1, 2, 3)
-    val b = a.map(x => x + 1)
-    b.foreach(x => print(x))*/
 
     /*
+    // 随机数
     for(i <- 1 to 10){
       val f = scala.util.Random.nextFloat().*(100)
     	println(i + "~" + f)
     }
     */
-    
-    /*
-    var x = 0
-    var sum:Double = 0
-    while (x < 60) {
-      if (sum < 100) {
-        sum += 8
-        x += 1
-        println(x + "-" + sum)
-      }
-      if (sum < 150 && sum > 100) {
-        sum += 6.4d
-        x += 1
-        println(x + "-" + sum)
-      }
-      if (sum >= 150) {
-        sum += 4
-        x += 1
-        println(x + "-" + sum)
-      }
-    }
-    */
-    
-    
-    
+
   }
 }
